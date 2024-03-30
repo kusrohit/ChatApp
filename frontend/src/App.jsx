3fa88d729789b6bfdf3fc20c2857b36404ed9272
kusrohit <kusrohit@users.noreply.github.com>
2024-03-30 12:47:05 +0530

    files update 1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,10 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import "./App.css";
+import { useAuthContext } from "./context/AuthContext";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import SignUp from "./pages/signup/SignUp";
-import { useAuthContext } from "./context/AuthContext";
 
 function App() {
   const {authUser, setAuthUser} = useAuthContext()
